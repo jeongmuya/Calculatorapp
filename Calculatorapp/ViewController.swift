1d1dd67f5f56439f0eb6235f6437468ada350249
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-12-24 19:25:47 +0900

    feat: Level1 완성

--- Calculatorapp/ViewController.swift
+++ Calculatorapp/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    private let label = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        
     }
-
-
+    
+    private func configureUI() {
+        view.backgroundColor = .black
+        
+        setupLabel()
+        setupConstraints()
+        
+    }
+    
+    // 12345
+    private func setupLabel() {
+        label.text = "12345"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 60, weight: .bold)
+        label.textAlignment = .right
+        
+        view.addSubview(label)
+    }
+    
+    // 12345 위치
+    
+    private func setupConstraints() {
+        label.snp.makeConstraints { make in
+            make.height.equalTo(100)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalToSuperview().inset(200)
+            
+        }
+        
+    }
+    
+    
+    
 }
+
 
